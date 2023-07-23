6381fccf4df9f000cb1931f5425be7574834d9c4
Oluwadamilola Oyenekan <adeniyioyenekan@yahoo.co.uk>
2023-07-23 04:07:50 +0100

    Add calculated surplus data to worksheet

--- run.py
+++ run.py
@@ -72,10 +72,18 @@ def calculate_surplus_data(sales_row):
     for stock, sale in zip(stock_row, sales_row):
         surplus = int(stock) - sale
         surplus_data.append(surplus)
-        print(f"Surplus for {stock} is {surplus}")
-    print(surplus_data)
+        
+    return surplus_data
 
 
+def update_surplus_worksheet(data):
+    '''
+    update surplus worksheet, add new row with list provided
+    '''
+    print("Updating surplus worksheet...\n")
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(data)
+    print("Surplus worksheet updated successfully")
 
 def main():
     '''
@@ -84,7 +92,9 @@ def main():
     data = get_sales_data()
     sales_data = [int(num) for num in data]
     update_sales_worksheet(sales_data)
-    calculate_surplus_data(sales_data)
+    new_surplus_data = calculate_surplus_data(sales_data)
+    update_surplus_worksheet(new_surplus_data)
+
 
 print("Welcome to Love Sandwiches Data Automation \n")
 main()
